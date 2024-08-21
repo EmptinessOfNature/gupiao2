f7b75b4f7592bf62d67cad13aef943e05e0655ef
EmptinessOfNature <lipengyubz@163.com>
2024-08-21 23:52:48 +0800

    增加短线和jw指标中

--- double_macd.py
+++ double_macd.py
@@ -57,10 +57,34 @@ def longport_kline(agent, period):
     print(1)
 
 
+def speed_abs(data, i, col, length):
+    if i < length:
+        return 0
+    return abs((data.loc[i - length, col] - data.loc[i, col]) / length)
+
+
+def speed(data, i, col, length):
+    if i < length:
+        return 0
+    return (data.loc[i - length, col] - data.loc[i, col]) / length
+
+
+def is_pos(data, i, col, length):
+    if i < length:
+        return 0
+    return (data.loc[i - length + 1 : i, col] > 0).all()
+
+
+def is_neg(data, i, col, length):
+    if i < length:
+        return 0
+    return (data.loc[i - length + 1 : i, col] < 0).all()
+
+
 def calc_buy_sell_point(data):
     # 判断是否是v形
     def is_v(data, i, col, pre_length, post_length, direction):
-        if i<pre_length+post_length:
+        if i < pre_length + post_length:
             return False
         i_seq = list(range(i - pre_length - post_length, i + 1))
         diffs = np.array(
@@ -83,7 +107,7 @@ def calc_buy_sell_point(data):
 
     # 判断是否穿0轴
     def is_chuan(data, i, col, pre_length, post_length, direction, thresh):
-        if i < pre_length+post_length:
+        if i < pre_length + post_length:
             return False
         i_seq = list(range(i - pre_length - post_length, i + 1))
         ret = np.array(data.loc[i_seq, col].reset_index(drop=True))
@@ -101,11 +125,6 @@ def calc_buy_sell_point(data):
             return True
         return False
 
-    def speed(data,i,col,length):
-        if i<length:
-            return 0
-        return abs((data.loc[i-length,col]-data.loc[i,col])/length)
-
     data["long"] = 0
     data["short"] = 0
     data["long_stop"] = 0
@@ -117,7 +136,7 @@ def calc_buy_sell_point(data):
             data.loc[i, "m1"] > 0
             and abs(data.loc[i, "m1"]) > 1
             and is_chuan(data, i, "m2", 1, 1, "up", 0)
-            and speed(data,i,"m2",2)>0.1
+            and speed_abs(data, i, "m2", 2) > 0.1
         )
         long_rules.append(
             data.loc[i, "m1"] > 0
@@ -128,9 +147,9 @@ def calc_buy_sell_point(data):
         )
         # 做多止盈止损条件
         long_stop_rules = []
-        long_stop_rules.append(is_chuan(data,i,"m2",1,1,"down",0))
+        long_stop_rules.append(is_chuan(data, i, "m2", 1, 1, "down", 0))
         long_stop_rules.append(is_v(data, i, "m2", 3, 3, "top"))
-        long_stop_rules.append(speed(data,i,"m2",2)<0.01)
+        long_stop_rules.append(speed_abs(data, i, "m2", 2) < 0.01)
 
         # 做空开单条件
         short_rules = []
@@ -138,7 +157,7 @@ def calc_buy_sell_point(data):
             data.loc[i, "m1"] < 0
             and abs(data.loc[i, "m1"]) > 1
             and is_chuan(data, i, "m2", 1, 1, "down", 0)
-            and speed(data, i, "m2", 2) > 0.1
+            and speed_abs(data, i, "m2", 2) > 0.1
         )
         short_rules.append(
             data.loc[i, "m1"] < 0
@@ -149,9 +168,9 @@ def calc_buy_sell_point(data):
         )
         # 做空止盈止损条件
         short_stop_rules = []
-        short_stop_rules.append(is_chuan(data,i,"m2",1,1,"up",0))
+        short_stop_rules.append(is_chuan(data, i, "m2", 1, 1, "up", 0))
         short_stop_rules.append(is_v(data, i, "m2", 3, 3, "bottom"))
-        short_stop_rules.append(speed(data,i,"m2",2)<0.01)
+        short_stop_rules.append(speed_abs(data, i, "m2", 2) < 0.01)
 
         if i <= 2:
             continue
@@ -171,6 +190,42 @@ def calc_buy_sell_point(data):
     return data
 
 
+def calc_point_duanxian_jw(data):
+    data["long_start"], data["long_end"], data["short_start"], data["short_end"] = (
+        0,
+        0,
+        0,
+        0,
+    )
+    for i in range(len(data)):
+        long_start_rules, long_end_rules, short_start_rules, short_end_rules = (
+            [],
+            [],
+            [],
+            [],
+        )
+        # 买卖点规则
+        long_start_rules.append(
+            is_pos(data, i, "duanxian", 2) and speed(data, i, "jw", 3) > 0
+        )
+        long_end_rules.append(is_neg(data, i, "duanxian", 2))
+        short_start_rules.append(
+            is_neg(data, i, "duanxian", 2) and speed(data, i, "jw", 3) < 0
+        )
+        short_end_rules.append(is_pos(data, i, "duanxian", 2))
+        if i < 2:
+            continue
+        if sum(long_start_rules) > 0:
+            data.loc[i,"long_start"] = 1
+        if sum(long_end_rules) > 0:
+            data.loc[i,"long_end"] = 1
+        if sum(short_start_rules) > 0:
+            data.loc[i,"short_start"] = 1
+        if sum(short_end_rules) > 0:
+            data.loc[i,"short_end"] = 1
+    return data
+
+
 def double_macd(data):
     m1 = MACD(data.close, 55, 89, 1)
     m2 = MACD(data.close, 13, 21, 1)
@@ -178,24 +233,62 @@ def double_macd(data):
     data["m2"] = m2[0]
     return data
 
+def jw(data):
+    N1 = 45
+    M1 = 15
+    M2 = 15
+    CLOSE = np.array(data["close"])
+    HIGH = np.array(data["high"])
+    LOW = np.array(data["low"])
+    RSV = (CLOSE - LLV(LOW, N1)) / (HHV(HIGH, N1) - LLV(LOW, N1)) * 100
+    K = SMA(RSV, M1, 1)
+    D = SMA(K, M2, 1)
+    JW = 3 * K - 2 * D
+    data["jw"] = JW
+    return data
+
+
+def duanxian(data):
+    try:
+        C = np.array(data["close"])
+        L = np.array(data["low"])
+        H = np.array(data["high"])
+        N = 35
+        M = 35
+        B1 = (HHV(H, N) - C) / (HHV(H, N) - LLV(L, N)) * 100 - M
+        B2 = SMA(B1, N, 1) + 100
+        B3 = (C - LLV(L, N)) / (HHV(H, N) - LLV(L, N)) * 100
+        B4 = SMA(B3, 3, 1)
+        B5 = SMA(B4, 3, 1) + 100
+        R6 = B5 - B2
+        data["duanxian"] = R6
+        return data
+    except:
+        print("短线操盘error")
+
+
 def huice(data):
-    data['long_in'],data['long_out'],data['short_in'],data['short_out']=0,0,0,0
+    data["long_in"], data["long_out"], data["short_in"], data["short_out"] = 0, 0, 0, 0
     # data['profit_rate'] = 0
 
-    long_in_ids = data[data['long']==1].index
-    short_in_ids = data[data['short']==1].index
+    long_in_ids = data[data["long"] == 1].index
+    short_in_ids = data[data["short"] == 1].index
     for id in long_in_ids:
-        find_area = data.iloc[id+1:]
-        first_long_end = find_area[find_area["long_stop"]==1].index[0]
-        data.loc[id,'long_in']=1
-        data.loc[first_long_end,"long_out"]=1
-        data.loc[first_long_end,'profit_rate'] = data.loc[first_long_end+1,'open']/data.loc[id+1,'open']-1
+        find_area = data.iloc[id + 1 :]
+        first_long_end = find_area[find_area["long_stop"] == 1].index[0]
+        data.loc[id, "long_in"] = 1
+        data.loc[first_long_end, "long_out"] = 1
+        data.loc[first_long_end, "profit_rate"] = (
+            data.loc[first_long_end + 1, "open"] / data.loc[id + 1, "open"] - 1
+        )
     for id in short_in_ids:
-        find_area = data.iloc[id + 1:]
+        find_area = data.iloc[id + 1 :]
         first_short_end = find_area[find_area["short_stop"] == 1].index[0]
-        data.loc[id, 'short_in'] = 1
+        data.loc[id, "short_in"] = 1
         data.loc[first_short_end, "short_out"] = 1
-        data.loc[first_short_end,'profit_rate'] = (data.loc[id+1,'open'] - data.loc[first_short_end+1,'open'])/data.loc[id+1,'open']
+        data.loc[first_short_end, "profit_rate"] = (
+            data.loc[id + 1, "open"] - data.loc[first_short_end + 1, "open"]
+        ) / data.loc[id + 1, "open"]
 
     return data
 
@@ -233,7 +326,9 @@ def draw_line(data, code=""):
         x=long_out_signals["dt_1D"],
         y=long_out_signals["close"] * 0.9,
         # text=long_out_signals['profit_rate'],
-        text = ['{:.1f}%'.format(p*100) for p in np.array(long_out_signals['profit_rate'])],
+        text=[
+            "{:.1f}%".format(p * 100) for p in np.array(long_out_signals["profit_rate"])
+        ],
         textposition="bottom center",
         textfont=dict(size=16, color="black"),
         mode="markers+text",
@@ -244,7 +339,10 @@ def draw_line(data, code=""):
         x=short_out_signals["dt_1D"],
         y=short_out_signals["close"] * 1.2,
         # text = short_out_signals['profit_rate'],
-        text=['{:.1f}%'.format(p * 100) for p in np.array(short_out_signals['profit_rate'])],
+        text=[
+            "{:.1f}%".format(p * 100)
+            for p in np.array(short_out_signals["profit_rate"])
+        ],
         textposition="bottom center",
         textfont=dict(size=16, color="black"),
         mode="markers+text",
@@ -265,12 +363,14 @@ def draw_line(data, code=""):
     fig.add_trace(kline_1D, row=1, col=1)
     fig.add_trace(trace_long, row=1, col=1)
     fig.add_trace(trace_short, row=1, col=1)
-    fig.add_trace(trace_long_stop,row=1,col=1)
-    fig.add_trace(trace_short_stop,row=1,col=1)
+    fig.add_trace(trace_long_stop, row=1, col=1)
+    fig.add_trace(trace_short_stop, row=1, col=1)
     fig.add_trace(go.Scatter(x=data["dt_1D"], y=data["m1"]), row=2, col=1)
     fig.add_trace(go.Scatter(x=data["dt_1D"], y=data["m2"]), row=2, col=1)
     fig.add_trace(
-        go.Scatter(x=long_in_signals["dt_1D"], y=[0] * len(long_in_signals), mode="markers"),
+        go.Scatter(
+            x=long_in_signals["dt_1D"], y=[0] * len(long_in_signals), mode="markers"
+        ),
         row=2,
         col=1,
     )
@@ -285,16 +385,19 @@ def draw_line(data, code=""):
     fig.update_layout(xaxis_rangeslider_visible=False)
     fig.update_layout(title_text=code)
     print(len(long_in_signals), len(short_in_signals))
-    fig.write_image("./data_huice_dm/fig.png")
+    # fig.write_image("./data_huice_dm/"+code+"_fig.png")
     fig.show()
 
 
 if __name__ == "__main__":
-    code = "AMD"
+    code = "PDD"
     data = tdx_raw2_kline("./data_tdx_raw/74#" + code + ".txt", period="1D")
     data = double_macd(data)
-    data = calc_buy_sell_point(data)
+    data = jw(data)
+    data = duanxian(data)
+    data = calc_point_duanxian_jw(data)
+    # data = calc_buy_sell_point(data)
     data = huice(data)
     data.to_csv("./data_huice_dm/" + code + ".csv")
-    draw_line(data, code)
-    print(data['profit_rate'].sum())
+    # draw_line(data, code)
+    print(data["profit_rate"].sum())
