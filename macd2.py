daa992d3a5f3a8e5cffc4b8154e40f5b9cf74daf
EmptinessOfNature <lipengyubz@163.com>
2024-08-29 00:29:50 +0800

    apply实现ing

--- macd2.py
+++ macd2.py
@@ -1,19 +1,30 @@
 from macd_utils import tdx_raw2_kline,double_macd,jw,duanxian
 
+def is_chuan_df(row,col,pre_len,post_len,thresh):
+    index = row.name
+    if index<pre_len+post_len-1:
+        return False
+    else:
+        values_pre = data[col].iloc[index-pre_len-post_len+1:index-post_len+1]
+        values_post = data[col].iloc[index-post_len+1:index+1]
+        ret = bool(((values_pre<=thresh).all()) & ((values_post>thresh).all()))
+        return ret
+
 def merge_signal(data):
-    long_rules.append(
-        data.loc[i, "m1"] > 0
-        and abs(data.loc[i, "m1"]) > 1
-        and is_chuan(data, i, "m2", 1, 1, "up", 0)
-        and speed_abs(data, i, "m2", 2) > 0.1
-    )
-    long_rules.append(
-        data.loc[i, "m1"] > 0
-        and abs(data.loc[i, "m1"]) > 1
-        and data.loc[i, "m2"] < 0
-        and is_v(data, i, "m2", 3, 3, "bottom")
-        and abs(data.loc[i, "m1"]) >= 2 * abs(data.loc[i, "m2"])
-    )
+    # long_rules.append(
+    #     data.loc[i, "m1"] > 0
+    #     and abs(data.loc[i, "m1"]) > 1
+    #     and is_chuan(data, i, "m2", 1, 1, "up", 0)
+    #     and speed_abs(data, i, "m2", 2) > 0.1
+    # )
+    # long_rules.append(
+    #     data.loc[i, "m1"] > 0
+    #     and abs(data.loc[i, "m1"]) > 1
+    #     and data.loc[i, "m2"] < 0
+    #     and is_v(data, i, "m2", 3, 3, "bottom")
+    #     and abs(data.loc[i, "m1"]) >= 2 * abs(data.loc[i, "m2"])
+    # )
+    data['debug_chuan'] = data.apply(is_chuan_df,axis=1,col='m1',pre_len=2,post_len=2,thresh=0)
     return data
 
 
@@ -23,4 +34,5 @@ if __name__=='__main__':
     data = double_macd(data)
     data = jw(data)
     data = duanxian(data)
+    data = merge_signal(data)
     print(1)
